6bdd282bc031fa61fd73ce54cbf9afda310410c3
Talovic <dzevad88@gmail.com>
2018-02-12 08:08:55 +0100

    Initial range and output components setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react';
 import './App.css';
+import Range from './components/Range';
+import Output from './components/Output';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      height: 170,
+      weight: 65,
+      bmi: 22.49,
+      bmiClass: 'Normal'
+    };
+  }
   render() {
     return (
       <div className="App">
-        <p>BMI calculator</p>
+        <h1>BMI Calculator</h1>
+        <form>
+          <div>
+            <label>Height</label>
+            <Range />
+          </div>
+          <div>
+            <label>Weight</label>
+          </div>
+        </form>
+        <br />
+        <br />
+        <Output />
       </div>
     );
   }
---
+++ src/components/Output.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Output extends Component {
+  render() {
+    return (
+      <div className="output">
+        <div>Test Output</div>
+      </div>
+    );
+  }
+}
+
+export default Output;
